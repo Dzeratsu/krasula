bbb544a3e359b4a86223eca2b2ff6e972533b973
Andrei Ivanov <an.ivanov@elma-bpm.com>
2024-05-07 01:00:11 +0400

    Coverage

--- components/AppCoverage.vue
+++ components/AppCoverage.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex lg:flex-row-reverse flex-col gap-[30px]">
       <div class="lg:w-5/12 lg:mx-0 mx-4">
-        <p class="uppercase font-semibold lg:text-[48px] lg:leading-[48px] text-[21px] max-sm:text-center w-8/12 mx-auto lg:ml-0 text-center lg:text-left">создает покрытие с гидрофобным эффектом</p>
+        <p class="uppercase font-semibold lg:text-[48px] lg:leading-[48px] text-[21px] max-sm:text-center lg:w-8/12 w-full mx-auto lg:ml-0 text-center lg:text-left">создает покрытие с гидрофобным эффектом</p>
         <p class="mt-6 text-[18px]">Эластичный защитный слой с гидрофобным эффектом отталкивает воду и грязь, не позволяет им проникать внутрь древесины, поверхность «самоочищается». Обработанные «Krasula» Aqua Top поверхности надолго сохраняют прочность и красивый внешний вид.</p>
       </div>
       <div class="lg:w-7/12 w-full">
@@ -12,10 +12,100 @@
 src="img/previeCoverage.png" width="670" height="403" class="w-full"/>
       </div>
     </div>
+    <div class="max-sm:hidden block">
+      <div class="flex flex-wrap mt-[56px] justify-around w-9/12 mx-auto">
+        <div v-for="(item) in slide" :key="item.text" class="w-[244px]">
+          <div class="flex flex-col justify-center items-center gap-x-[86px]">
+            <nuxt-img :src="item.imgSrc" width="180" height="180" class="align-middle"/>
+            <div
+              class="font-semibold text-[18px] text-center mt-4">
+<!--              :class="[index > 0 ? 'mt-4': '']"-->
+            {{ item.text }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="swiper max-sm:block hidden">
+      <ul class="swiper-wrapper mt-4">
+        <li
+          v-for="item in slide"
+          :key="item.text"
+          class="swiper-slide flex flex-col items-center text-center "
+        >
+          <nuxt-img
+            width="193"
+            heigh="193"
+            format="webp"
+            :alt="item.text"
+            quality="100" :src="item.imgSrc" />
+          <span class="mt-[21px] text-[21px] text-center font-semibold w-11/12">{{item.text}}</span>
+        </li>
+      </ul>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'AppCoverage'
+  name: 'AppCoverage',
+  data() {
+    return {
+      slide: [
+        { imgSrc: 'img/coverage/coverage1.png', text: 'Гидрофобный эффект'},
+        { imgSrc: 'img/coverage/coverage2.png', text: 'Бесцветное, шелковистое полуматовое покрытие, подчеркивает текстуру дерева' },
+        { imgSrc: 'img/coverage/coverage3.png', text: 'Обработанные поверхности безопасны для людей и животных' },
+      ],
+    }
+  },
+  mounted() {
+    /* eslint-disable no-unused-vars */
+    const swiper = new this.$swiper('.swiper2', {
+      grabCursor: true,
+      modules: [this.$swiperModules.Navigation, this.$swiperModules.Pagination],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      spaceBetween: 16,
+    })
+  },
 }
 </script>
+
+<style>
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  object-fit: cover;
+}
+.swiper-button-next {
+  font-weight: 900;
+  margin-top: -100px;
+  color: #ffee00;
+  width: 15px;
+}
+.swiper-button-prev {
+  font-weight: 900;
+  margin-top: -100px;
+  color: #ffee00;
+}
+.swiper-button-next.swiper-button-disabled, .swiper-button-prev.swiper-button-disabled {
+  color: #545454;
+  cursor: auto;
+  pointer-events: none;
+}
+</style>
+
